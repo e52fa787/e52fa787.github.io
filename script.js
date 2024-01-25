ee5bc33169fdac439b24befebf3d4e5eb67bcded
e52fa787 <tiansh02@gmail.com>
2024-01-26 00:44:39 +0800

    Added input styling for control panel

--- script.js
+++ script.js
@@ -1,13 +1,59 @@
-(function($w){
-const $doc=$w.document,
+(function(global, factory){global.onload=factory})(typeof window!=='undefined'?window:this, function(globalLoadEvent){'use strict';
+
+const $w=globalLoadEvent.currentTarget,
+    $doc=window.document,
     $body=$doc.body
-let $controls=$doc.getElementById('control-panel')
-let $canvas=$doc.getElementById('canvas')
+
+
+
+const HASTOUCHEVENTS = typeof $w.ontouchstart!=='undefined'
+
+let $controls=$doc.getElementById('controls')
 
 let $controlsDragTrigger=$controls.getElementsByTagName('header')[0]
+let $controlTogglePanel=$controls.getElementsByClassName('toggle-dropdown')[0]
+let $controlToggleAnimation=$controls.getElementsByClassName('toggle-animation')[0]
 
 let dragPos=[0,0]
 let controlPos=[$controls.offsetLeft,$controls.offsetTop]
+
+let EVENTDRAGMOVE='mousemove',
+    EVENTDRAGEND='mouseup',
+    EVENTDRAGSTART='mousedown'
+
+if(HASTOUCHEVENTS){
+    EVENTDRAGEND='touchend'
+    EVENTDRAGMOVE='touchmove'
+    EVENTDRAGSTART='touchstart'
+}
+
+let $canvas=$doc.getElementById('canvas'),
+    ctx=$canvas.getContext('2d')
+
+let currentAnimationFrame,
+    animationEnabled=true
+
+// CONTROL PANEL
+
+/**
+ * Extracts [x,y] coordinates from a touch/mouse event
+ * @param {Event} e 
+ * @returns {Array} Array of two coordinates contained in the event
+ */
+
+let extractCoordsFromEvent=function(e){
+    let pos=[0,0]
+    if(HASTOUCHEVENTS) {
+        const { touches, changedTouches } = e.originalEvent ?? e;
+        const touch = touches[0] ?? changedTouches[0];
+        pos[0] = touch.clientX;
+        pos[1] = touch.clientY;
+    }else{
+        pos[0] = e.clientX;
+        pos[1] = e.clientY;
+    }
+    return pos
+}
 
 /**
  * Event listener for events that trigger $controls dragging
@@ -16,16 +62,17 @@ let controlPos=[$controls.offsetLeft,$controls.offsetTop]
 let handleControlsDragStart=function(e){
     e=e||windows.event
     e.preventDefault()
-    dragPos[0]=e.clientX
-    dragPos[1]=e.clientY
+
+    dragPos = extractCoordsFromEvent(e)
+
     $controls.classList.add('currentlyDragging')
 
-    $doc.addEventListener('mouseup', handleControlsDragEnd)
+    $doc.addEventListener(EVENTDRAGEND, handleControlsDragEnd)
     $w.addEventListener('blur', handleControlsDragEnd)
-    $doc.addEventListener('mousemove', handleControlsDragMove)
+    $doc.addEventListener(EVENTDRAGMOVE, handleControlsDragMove)
 }
 
-$controlsDragTrigger.addEventListener('mousedown', handleControlsDragStart)
+$controlsDragTrigger.addEventListener(EVENTDRAGSTART, handleControlsDragStart)
 
 /**
  * Event listener for move events during $controls dragging.
@@ -34,30 +81,46 @@ $controlsDragTrigger.addEventListener('mousedown', handleControlsDragStart)
 
 let handleControlsDragMove=function(e){
     e=e||windows.event
-    e.preventDefault()
 
-    controlPos[0]=controlPos[0] + e.clientX - dragPos[0]
-    controlPos[1]=controlPos[1] + e.clientY - dragPos[1]
+    let [x,y]=extractCoordsFromEvent(e),
+        computedLeft,
+        computedTop
 
-    dragPos[0]=e.clientX
-    dragPos[1]=e.clientY
+    // compute new positions based on mouse movement
+
+    controlPos[0]=controlPos[0] + x - dragPos[0]
+    controlPos[1]=controlPos[1] + y - dragPos[1]
+
+    //store old positions
+
+    dragPos[0]=x
+    dragPos[1]=y
 
     //$controlsDragTrigger.innerHTML=dragPos[0]+', '+dragPos[1]
 
+    // Move $controls horizontally
+
     if(controlPos[0] > 0 && controlPos[0] + $controls.clientWidth <= $body.clientWidth){
-        $controls.style.left=controlPos[0] + 'px'
+        computedLeft=controlPos[0]
     }else if(controlPos[0] < 0){
-        $controls.style.left='0'
+        computedLeft=0
     }else if(controlPos[0] + $controls.clientWidth > $body.clientWidth){
-        $controls.style.left=($body.clientWidth-$controls.clientWidth) + 'px'
+        computedLeft=$body.clientWidth-$controls.clientWidth
     }
+
+    // Move $controls vertically
+
     if(controlPos[1] > 0 && controlPos[1] + $controls.clientHeight <= $body.clientHeight){
-        $controls.style.top=controlPos[1] + 'px'
+        computedTop=controlPos[1]
     }else if(controlPos[1] < 0){
-        $controls.style.top='0'
+        computedTop=0
     }else if(controlPos[1] + $controls.clientHeight > $body.clientHeight){
-        $controls.style.top=($body.clientHeight-$controls.clientHeight) + 'px'
+        computedTop=$body.clientHeight-$controls.clientHeight
     }
+
+    $controls.style.transform='translate('+computedLeft+'px,'+computedTop+'px)'
+
+
 }
 
 /**
@@ -65,9 +128,46 @@ let handleControlsDragMove=function(e){
  */
 let handleControlsDragEnd=function(){
     $controls.classList.remove('currentlyDragging')
-    $doc.removeEventListener('mouseup', handleControlsDragEnd)
+    $doc.removeEventListener(EVENTDRAGEND, handleControlsDragEnd)
     $w.removeEventListener('blur', handleControlsDragEnd)
-    $doc.removeEventListener('mousemove', handleControlsDragMove)
+    $doc.removeEventListener(EVENTDRAGMOVE, handleControlsDragMove)
+}
+
+let handleControlPanelToggle=function(){
+    $controls.classList.toggle('closed')
+}
+
+$controlTogglePanel.addEventListener('click', handleControlPanelToggle)
+
+let handleAnimationToggle=function(e){
+    if(animationEnabled){
+        animationEnabled=false
+        $w.cancelAnimationFrame(currentAnimationFrame)
+    }else{
+        animationEnabled=true
+        $w.requestAnimationFrame(animate)
+    }
+}
+
+$controlToggleAnimation.addEventListener('change',handleAnimationToggle)
+
+
+
+
+
+
+
+
+
+// CANVAS STUFF
+
+var handleResize=function(){
+    width=$w.innerWidth;
+
+}
+
+var animate=function(){
+    currentAnimationFrame=$w.requestAnimationFrame(animate)
 }
 
 
@@ -80,4 +180,4 @@ let handleControlsDragEnd=function(){
 
 
 
-})(window)
+})
